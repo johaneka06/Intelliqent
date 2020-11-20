f0c2569ad8474e7b9888332be4a48980a2cffab2
Johan Santosa <johanekasantosa@yahoo.com>
2020-11-20 16:20:09 +0700

    Change navbar

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,21 +1,39 @@
 <nav class="navbar navbar-expand-lg navbar-light">
-  <a class="navbar-brand" href="{{ url('/') }}">
-    <img src="{{ asset('logo/intelliqent.png') }}" width="50" height="50" alt="" loading="lazy">
-    <h3 class="d-inline ml-1 align-middle"><strong>INTELLIQENT</strong></h3>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
-    <div class="navbar-nav ml-auto">
-      <a class="nav-link mr-4" href="{{ url('/') }}" id="home"><strong class="navbar-hover-color">Home</strong></a>
-      <a class="nav-link mr-4" href="#" id="learn"><strong class="navbar-hover-color">Learn</strong></a>
-      <a class="nav-link mr-4" href="#" id="forum"><strong class="navbar-hover-color">Forum</strong></a>
-      @if(!Auth::user())
-      <a class="nav-link mr-4" href="{{ url('/login') }}" id="login"><strong class="navbar-hover-color">Members</strong></a>
-      @else
-      <a class="nav-link mr-4" href="{{ url('/member/'.Auth::user()->id) }}" id="member"><strong class="navbar-hover-color">{{ Auth::user()->username }}</strong></a>
-      @endif
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="{{ asset('logo/intelliqent.png') }}" width="50" height="50" alt="" loading="lazy">
+        <h3 class="d-inline ml-1 align-middle"><strong>INTELLIQENT</strong></h3>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="{{ url('/') }}" id="home"><strong class="navbar-hover-color">Home</strong></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="#" id="learn"><strong class="navbar-hover-color">Learn</strong></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="#" id="forum"><strong class="navbar-hover-color">Forum</strong></a>
+            </li>
+            @if(!Auth::user())
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="{{ url('/login') }}" id="login"><strong class="navbar-hover-color">Members</strong></a>
+            </li>
+            @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <strong>{{ Auth::user()->username }}</strong>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="{{ url('/member/'.Auth::user()->id) }}">View Profile</a>
+                    <a class="dropdown-item" href="{{ url('/logout') }}">Log Out</a>
+                </div>
+            </li>
+
+            @endif
+
+        </ul>
     </div>
-  </div>
 </nav>
