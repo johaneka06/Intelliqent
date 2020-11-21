ea21be377bbc7746bee7d76f0b441d2308667cfc
Jonathan Wijaya <gevin2000@gmail.com>
2020-11-21 15:01:00 +0700

    hias preferences

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,5 +3,11 @@
 @section('page-title','Home | Intelliqent')
 
 @section('page-content')
-
+<div class="title-background justify-content-center align-items-center">
+    <div class="center-content">
+        <h1 class="font-weight-bolder text-white big-and-bold">Learn anything, wherever, whenever.</h1>
+        
+    </div>
+    
+</div>
 @endsection
--- resources/views/select-preference.blade.php
+++ resources/views/select-preference.blade.php
@@ -4,20 +4,33 @@
 @section('page-content')
 
 <div class="container mt-4">
-  <h3 class="text-center">Select your content preferences</h3>
+  <h3 class="text-center mb-5">Select your content preferences</h3>
 
   <form action="{{ url('/register/preferences') }}" method="post">
     @csrf
     
     @foreach($categories as $category)
-    <input type="checkbox" name="preferences[]" value="{{$category->id}}"> {{$category->category_name}}
+    @if ($category->id%2==0)
+      <div class="d-block pt-3 pb-2">
+      <input type="checkbox" name="preferences[]" value="{{$category->id}}" class="mr-3 ml-3" id="{{$category->id}}">
+        <label for="{{$category->id}}">{{$category->category_name}}</label>
+      </div>
+    @else
+      <div class="d-block pt-3 pb-2 bg-light">
+        <input type="checkbox" name="preferences[]" value="{{$category->id}}" class="mr-3 ml-3" id="{{$category->id}}"> 
+        <label for="{{$category->id}}">{{$category->category_name}}</label>
+      </div>
+        
+    @endif
+    
+    
     @endforeach
     <div class="text-center">
-      <button type="submit" class="btn btn-primary">Save</button>
+      <button type="submit" class="btn btn-primary mb-3 mt-5">Save</button>
     </div>
   </form>
 
-  <small class="text-muted">You may select preferences later</small>
+  <small class="text-muted d-flex justify-content-center">You may select preferences later</small>
 </div>
 
 @endsection
