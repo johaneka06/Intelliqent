69a359bd4f9ad8141fd145895b612bb3d0f59bbb
Johan Santosa <johanekasantosa@yahoo.com>
2020-11-23 14:41:50 +0700

    fix homepage layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,17 +4,20 @@
 
 @section('page-content')
 <div class="title-background justify-content-center align-items-center">
-    <div class="center-content">
-        <h1 class="font-weight-bolder text-white big-and-bold center-self">Learn anything,<br>wherever, whenever.</h1>
-        @if (!Auth::user())
-            <div class="center-self mt-5">
-                <a href="{{ url('/login') }}" class="btn btn-primary font-weight-bold">Start your journey</a>
-            </div>
-            
-        @else
-            <a href="" class="btn btn-primary font-weight-bold">Start your journey</a>
-        @endif
+  <div class="center-content">
+    <h1 class="font-weight-bolder text-white big-and-bold center-self">Learn anything,<br>wherever, whenever.</h1>
+    @if (!Auth::user())
+    <div class="center-self mt-5">
+      <a href="{{ url('/login') }}" class="btn btn-primary font-weight-bold">Start your journey</a>
     </div>
-    
+
+    @else
+    <div class="center-self mt-5">
+      <a href=" {{ url('/course') }} " class=" btn btn-primary font-weight-bold">Start your journey</a>
+    </div>
+
+    @endif
+  </div>
+
 </div>
 @endsection
