5af57dd3ddd14d88464d683fad998f80ff4eef39
Jonathan Wijaya <gevin2000@gmail.com>
2020-11-23 14:22:31 +0700

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,15 @@
 @section('page-content')
 <div class="title-background justify-content-center align-items-center">
     <div class="center-content">
-        <h1 class="font-weight-bolder text-white big-and-bold">Learn anything, wherever, whenever.</h1>
-        
+        <h1 class="font-weight-bolder text-white big-and-bold center-self">Learn anything,<br>wherever, whenever.</h1>
+        @if (!Auth::user())
+            <div class="center-self mt-5">
+                <a href="{{ url('/login') }}" class="btn btn-primary font-weight-bold">Start your journey</a>
+            </div>
+            
+        @else
+            <a href="" class="btn btn-primary font-weight-bold">Start your journey</a>
+        @endif
     </div>
     
 </div>
