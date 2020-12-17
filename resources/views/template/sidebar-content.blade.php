538a5d0c430f52c42fb786e384c654e3252034d0
Johan Santosa <johanekasantosa@yahoo.com>
2020-12-17 11:19:24 +0700

    Fix front end

--- resources/views/template/sidebar-content.blade.php
+++ resources/views/template/sidebar-content.blade.php
@@ -1,6 +1,6 @@
 @foreach($categories as $category)
 <div class="form-inline">
-  <input type="checkbox" name="category[]" id="category[]" value="{{$category->id}}" class="mr-3 ml-3" {{ request()->filled('category') ? ( in_array($category->id, request()->category) ? 'checked' : '') :''}}>
-  <label for="category[]">{{ $category->category_name }}</label>
+  <input type="checkbox" name="category[]" id="{{ $category->category_name }}" value="{{$category->id}}" class="mr-3 ml-3" {{ request()->filled('category') ? ( in_array($category->id, request()->category) ? 'checked' : '') :''}}>
+  <label for="{{ $category->category_name }}">{{ $category->category_name }}</label>
 </div>
 @endforeach
